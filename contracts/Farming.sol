a23d444213c985ddfcdcc44aa2a0a2647cddbaee
manoj <manoj@noborders.net>
2022-01-09 16:04:20 +0530

    commenting added

--- contracts/Farming.sol
+++ contracts/Farming.sol
@@ -7,7 +7,9 @@ import "./Ownable.sol";
 
 /// @title A farming smart contract
 /// @author M.Armaghan Raza
-/// @notice This smart contract serves as a farming pool where users can stake against and get rewards.
+/// @notice This smart contract serves as a farming pool where users can farm against and get rewards.
+/// @dev All function calls are currently implemented without side effects.
+// @custom:experimental This contract is experimental.
 contract Farming is Ownable {
     using SafeMath for uint;
     
@@ -16,7 +18,7 @@ contract Farming is Ownable {
 
     /******************* State Variables **********************/
     /// @notice This struct stores information regarding farming info details.
-    struct stakingInfo {
+    struct farmingInfo {
         uint amount;
     }
 
@@ -29,153 +31,165 @@ contract Farming is Ownable {
     //allowed token addresses
     mapping (address => bool) public allowedTokens;
     
-    // @notice tokenAddr to user to stake amount.
-    mapping (address => mapping(address => stakingInfo)) public StakeMap;
+    // @notice tokenAddr to user to farm amount.
+    mapping (address => mapping(address => farmingInfo)) public FarmMap;
 
     // @notice mapping for tokenAddress for Reward.
     mapping (address => rewardInfo) public rewardAddrMap;
     
-    // @notice tokenAddr to user to remaining claimable amount per stake.
-    mapping (address => mapping(address => uint)) public userCummRewardPerStake;
+    // @notice tokenAddr to user to remaining claimable amount per farm.
+    mapping (address => mapping(address => uint)) public userCummRewardPerFarm;
 
     // @notice tokenAddr to cummulative per token reward since the beginning or time.
-    mapping (address => uint) public tokenCummRewardPerStake;
+    mapping (address => uint) public tokenCummRewardPerFarm;
 
     // @notice tokenAddr to total token claimed.
-    mapping (address => uint) public tokenTotalStaked;
-  
+    mapping (address => uint) public tokenTotalFarmed;
+    
+    // @notice Stores farm tokenAddress.
+    address public StakeTokenAddr;
+    
+    constructor(address _tokenAddr) public{
+        StakeTokenAddr= _tokenAddr;
+    }
+
+    /******************* Events **********************/
+    event farmed(
+        address _token,
+        address sender,
+        uint256 _amount
+    );
+
+    event claimed(
+        address _token,
+        address sender,
+        uint256 _amount,
+        address receiver
+    );
+
+    event unfarmed(
+        address _token,
+        address sender,
+        uint256 _amount
+    );
+
+    /******************* Modifiers **********************/
     // @notice modifieer to validate TokenAddress.    
     modifier isValidToken(address _tokenAddr){
         require(allowedTokens[_tokenAddr]);
         _;
     }
 
-    // @notice Stores stake tokenAddress.
-    address public StakeTokenAddr;
     
-    constructor(address _tokenAddr) public{
-        StakeTokenAddr= _tokenAddr;
-    }
-    
-    
-    /**
-    * @dev add approved token address to the mapping 
-    */
-    
+    /******************* Admin Methods **********************/
+
+    /// @notice Admin method to add LP Address Farming
+    /// @param _tokenAddr LP address
     function addToken( address _tokenAddr) onlyOwner external {
         allowedTokens[_tokenAddr] = true;
     }
     
-    /**
-    * @dev remove approved token address from the mapping 
-    */
+    /// @notice Admin method to remove LP Address Farming
+    /// @param _tokenAddr LP address
     function removeToken( address _tokenAddr) onlyOwner external {
         allowedTokens[_tokenAddr] = false;
     }
 
-    /**
-    * @dev harvest a specific amount to a token
-    * @param _amount the amount to be staked
-    * @param _tokenAddr the token the user wish to harvest on
-    * for demo purposes, not requiring user to actually send in tokens right now
-    */
-    
-    function harvest(uint _amount, address _tokenAddr) isValidToken(_tokenAddr) external returns (bool){
-        require(_amount != 0);
-        require(ERC20(StakeTokenAddr).transferFrom(msg.sender,this,_amount));
-        
-        if (StakeMap[_tokenAddr][msg.sender].amount ==0){
-            StakeMap[_tokenAddr][msg.sender].amount = _amount;
-            userCummRewardPerStake[_tokenAddr][msg.sender] = tokenCummRewardPerStake[_tokenAddr];
-        }else{
-            claim(_tokenAddr, msg.sender);
-            StakeMap[_tokenAddr][msg.sender].amount = StakeMap[_tokenAddr][msg.sender].amount.add( _amount);
-        }
-        tokenTotalStaked[_tokenAddr] = tokenTotalStaked[_tokenAddr].add(_amount);
-        return true;
+    /// @notice Admin method to set rewardTokenAddress against the Lp Address Farming
+    /// @param _tokenAddr LP address
+    /// @param _rewardAddress rewardTokenAddress
+    function rewardAddress(address _tokenAddr, address _rewardAddress) isValidToken(_tokenAddr)  external onlyOwner {
+        rewardAddrMap[_tokenAddr]._rewardTokenAddress = _rewardAddress;
     }
-    
-    
-    /**
-     * demo version
-    * @dev pay out dividends to stakers, update how much per token each staker can claim
-    * @param _reward the aggregate amount to be send to all stakers
-    * @param _tokenAddr the token that this dividend gets paied out in
-    */
+
+    /// @notice Admin method to set pay out dividends to farmers, update how much per token each farmer can claim
+    /// @param _reward the aggregate amount to be send to all farmers
+    /// @param _tokenAddr the token that this dividend gets paied out in
     function distribute(uint _reward,address _tokenAddr) isValidToken(_tokenAddr) external onlyOwner returns (bool){
-        require(tokenTotalStaked[_tokenAddr] != 0);
+        require(tokenTotalFarmed[_tokenAddr] != 0);
         uint reward = _reward.mul(BIGNUMBER); //simulate floating point operations
-        uint rewardAddedPerToken = reward.div(tokenTotalStaked[_tokenAddr]);
-        tokenCummRewardPerStake[_tokenAddr] = tokenCummRewardPerStake[_tokenAddr].add(rewardAddedPerToken);
+        uint rewardAddedPerToken = reward.div(tokenTotalFarmed[_tokenAddr]);
+        tokenCummRewardPerFarm[_tokenAddr] = tokenCummRewardPerFarm[_tokenAddr].add(rewardAddedPerToken);
         return true;
     }
     
     // /**
     // * production version
-    // * @dev pay out dividends to stakers, update how much per token each staker can claim
-    // * @param _reward the aggregate amount to be send to all stakers
+    // * @dev pay out dividends to farmers, update how much per token each farmer can claim
+    // * @param _reward the aggregate amount to be send to all farmers
     // */
     
     // function distribute(uint _reward) isValidToken(msg.sender) external returns (bool){
-    //     require(tokenTotalStaked[msg.sender] != 0);
+    //     require(tokenTotalFarmed[msg.sender] != 0);
     //     uint reward = _reward.mul(BIGNUMBER);
-    //     tokenCummRewardPerStake[msg.sender] += reward/tokenTotalStaked[msg.sender];
+    //     tokenCummRewardPerFarm[msg.sender] += reward/tokenTotalFarmed[msg.sender];
     //     return true;
     // } 
+
+    /******************* Public Methods **********************/
+    /// @notice This method to harvest a specific amount against LP Token
+    /// @param  _amount the amount to be farmed
+    /// @param _tokenAddr the token the user wish to harvest on
+    function harvest(uint _amount, address _tokenAddr) isValidToken(_tokenAddr) external returns (bool){
+        require(_amount != 0);
+        require(ERC20(_tokenAddr).transferFrom(msg.sender,this,_amount));
+        
+        if (FarmMap[_tokenAddr][msg.sender].amount ==0){
+            FarmMap[_tokenAddr][msg.sender].amount = _amount;
+            userCummRewardPerFarm[_tokenAddr][msg.sender] = tokenCummRewardPerFarm[_tokenAddr];
+        }else{
+            claim(_tokenAddr, msg.sender);
+            FarmMap[_tokenAddr][msg.sender].amount = FarmMap[_tokenAddr][msg.sender].amount.add( _amount);
+        }
+        tokenTotalFarmed[_tokenAddr] = tokenTotalFarmed[_tokenAddr].add(_amount);
+        emit farmed(_tokenAddr, msg.sender, _amount);
+        return true;
+    }
     
-    
-    event claimed(uint amount);
-    /**
-    * @dev claim dividends for a particular token that user has stake in
-    * @param _tokenAddr the token that the claim is made on
-    * @param _receiver the address which the claim is paid to
-    */
+    /// @notice This method to claim dividends for a particular token that user has farmed in
+    /// @param  _tokenAddr the token that the claim is made on
+    /// @param _receiver the address which the claim is paid to
     function claim(address _tokenAddr, address _receiver) isValidToken(_tokenAddr)  public returns (uint) {
         address token = rewardAddrMap[_tokenAddr]._rewardTokenAddress;
-        uint stakedAmount = StakeMap[_tokenAddr][msg.sender].amount;
+        uint stakedAmount = FarmMap[_tokenAddr][msg.sender].amount;
         //the amount per token for this user for this claim
-        uint amountOwedPerToken = tokenCummRewardPerStake[_tokenAddr].sub(userCummRewardPerStake[_tokenAddr][msg.sender]);
+        uint amountOwedPerToken = tokenCummRewardPerFarm[_tokenAddr].sub(userCummRewardPerFarm[_tokenAddr][msg.sender]);
         uint claimableAmount = stakedAmount.mul(amountOwedPerToken); //total amoun that can be claimed by this user
         claimableAmount = claimableAmount.mul(DECIMAL); //simulate floating point operations
         claimableAmount = claimableAmount.div(BIGNUMBER); //simulate floating point operations
-        userCummRewardPerStake[_tokenAddr][msg.sender]=tokenCummRewardPerStake[_tokenAddr];
+        userCummRewardPerFarm[_tokenAddr][msg.sender]=tokenCummRewardPerFarm[_tokenAddr];
         if (_receiver == address(0)){
             require(ERC20(token).transfer(msg.sender,claimableAmount));
         } else {
            require(ERC20(token).transfer(_receiver,claimableAmount));
         }
-        emit claimed(claimableAmount);
+        emit claimed(_tokenAddr, msg.sender, claimableAmount, _receiver);
         return claimableAmount;
 
     }
-    
+
+    /// @notice This method to public function to check the claim amount
+    /// @param  _tokenAddr the token that the claim is made on
+    /// @param _receiver the address which the claim is paid to
     function claimReward(address _tokenAddr, address _receiver) isValidToken(_tokenAddr)  public view returns (uint, uint, uint) {
-        address token = rewardAddrMap[_tokenAddr]._rewardTokenAddress;
-        uint stakedAmount = StakeMap[_tokenAddr][_receiver].amount;
+        uint stakedAmount = FarmMap[_tokenAddr][_receiver].amount;
         //the amount per token for this user for this claim
-        uint amountOwedPerToken = tokenCummRewardPerStake[_tokenAddr].sub(userCummRewardPerStake[_tokenAddr][msg.sender]);
+        uint amountOwedPerToken = tokenCummRewardPerFarm[_tokenAddr].sub(userCummRewardPerFarm[_tokenAddr][msg.sender]);
         uint claimableAmount = stakedAmount.mul(amountOwedPerToken); //total amoun that can be claimed by this user
         // claimableAmount = claimableAmount.mul(DECIMAL); //simulate floating point operations
         // claimableAmount = claimableAmount.div(BIGNUMBER); //simulate floating point operations
         return (stakedAmount, amountOwedPerToken, claimableAmount);
     }
 
-    /**
-    * @dev Unharvest
-    */
+    /// @notice This method to unharvest a specific amount against LP Token
+    /// @param  _amount the amount to be unfarmed
+    /// @param _tokenAddr the token the user wish to harvest on
     function Unharvest(uint _amount, address _tokenAddr) isValidToken(_tokenAddr)  external returns(bool){
-        require(StakeMap[_tokenAddr][msg.sender].amount >0 );
+        require(FarmMap[_tokenAddr][msg.sender].amount >0 );
         claim(_tokenAddr, msg.sender);
         require(ERC20(_tokenAddr).transfer(msg.sender,_amount));
-        tokenTotalStaked[_tokenAddr] = tokenTotalStaked[_tokenAddr].sub(_amount);
+        tokenTotalFarmed[_tokenAddr] = tokenTotalFarmed[_tokenAddr].sub(_amount);
+        emit unfarmed(_tokenAddr, msg.sender, _amount);
         return true;
     }
-
-    /**
-    * @dev rewardAddress
-    */
-    function rewardAddress(address _tokenAddr, address _rewardAddress) isValidToken(_tokenAddr)  external onlyOwner {
-        rewardAddrMap[_tokenAddr]._rewardTokenAddress = _rewardAddress;
-    }
-    
 }
--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -8,6 +8,8 @@ import "./Ownable.sol";
 /// @title A staking smart contract
 /// @author M.Armaghan Raza
 /// @notice This smart contract serves as a staking pool where users can stake against and get rewards.
+/// @dev All function calls are currently implemented without side effects.
+// @custom:experimental This contract is experimental.
 contract Staking is Ownable {
     using SafeMath for uint;
     
@@ -36,12 +38,6 @@ contract Staking is Ownable {
     // @notice tokenAddr to total token claimed.
     mapping (address => uint) public tokenTotalStaked;
   
-    // @notice modifieer to validate TokenAddress.    
-    modifier isValidToken(address _tokenAddr){
-        require(allowedTokens[_tokenAddr]);
-        _;
-    }
-
     // @notice Stores stake tokenAddress.
     address public StakeTokenAddr;
     
@@ -49,55 +45,54 @@ contract Staking is Ownable {
         StakeTokenAddr= _tokenAddr;
     }
     
+    /******************* Events **********************/
+    event staked(
+        address _token,
+        address sender,
+        uint256 _amount
+    );
+
+    event claimed(
+        address _token,
+        address sender,
+        uint256 _amount,
+        address receiver
+    );
+
+    event unstaked(
+        address _token,
+        address sender,
+        uint256 _amount
+    );
     
-    /**
-    * @dev add approved token address to the mapping 
-    */
-    
+    /******************* Modifiers **********************/
+    // @notice modifieer to validate TokenAddress.    
+    modifier isValidToken(address _tokenAddr){
+        require(allowedTokens[_tokenAddr]);
+        _;
+    }
+
+    /******************* Admin Methods **********************/
+
+    /// @notice Admin method to add LP Address Farming
+    /// @param _tokenAddr LP address
     function addToken( address _tokenAddr) onlyOwner external {
         allowedTokens[_tokenAddr] = true;
     }
     
-    /**
-    * @dev remove approved token address from the mapping 
-    */
+    /// @notice Admin method to remove LP Address Farming
+    /// @param _tokenAddr LP address
     function removeToken( address _tokenAddr) onlyOwner external {
         allowedTokens[_tokenAddr] = false;
     }
 
-    /**
-    * @dev stake a specific amount to a token
-    * @param _amount the amount to be staked
-    * @param _tokenAddr the token the user wish to stake on
-    * for demo purposes, not requiring user to actually send in tokens right now
-    */
-    
-    function stake(uint _amount, address _tokenAddr) isValidToken(_tokenAddr) external returns (bool){
-        require(_amount != 0);
-        require(ERC20(StakeTokenAddr).transferFrom(msg.sender,this,_amount));
-        
-        if (StakeMap[_tokenAddr][msg.sender].amount ==0){
-            StakeMap[_tokenAddr][msg.sender].amount = _amount;
-            userCummRewardPerStake[_tokenAddr][msg.sender] = tokenCummRewardPerStake[_tokenAddr];
-        }else{
-            claim(_tokenAddr, msg.sender);
-            StakeMap[_tokenAddr][msg.sender].amount = StakeMap[_tokenAddr][msg.sender].amount.add( _amount);
-        }
-        tokenTotalStaked[_tokenAddr] = tokenTotalStaked[_tokenAddr].add(_amount);
-        return true;
-    }
-    
-    
-    /**
-     * demo version
-    * @dev pay out dividends to stakers, update how much per token each staker can claim
-    * @param _reward the aggregate amount to be send to all stakers
-    * @param _tokenAddr the token that this dividend gets paied out in
-    */
+    /// @notice Admin method to set pay out dividends to farmers, update how much per token each farmer can claim
+    /// @param _reward the aggregate amount to be send to all farmers
+    /// @param _tokenAddr the token that this dividend gets paied out in
     function distribute(uint _reward,address _tokenAddr) isValidToken(_tokenAddr) external onlyOwner returns (bool){
         require(tokenTotalStaked[_tokenAddr] != 0);
         uint reward = _reward.mul(BIGNUMBER); //simulate floating point operations
-        uint rewardAddedPerToken = reward/tokenTotalStaked[_tokenAddr];
+        uint rewardAddedPerToken = reward.div(tokenTotalStaked[_tokenAddr]);
         tokenCummRewardPerStake[_tokenAddr] = tokenCummRewardPerStake[_tokenAddr].add(rewardAddedPerToken);
         return true;
     }
@@ -113,15 +108,31 @@ contract Staking is Ownable {
     //     uint reward = _reward.mul(BIGNUMBER);
     //     tokenCummRewardPerStake[msg.sender] += reward/tokenTotalStaked[msg.sender];
     //     return true;
-    // } 
+    // }
+
+    /******************* Public Methods **********************/
+    /// @notice This method to stake a specific amount against Token
+    /// @param  _amount the amount to be farmed
+    /// @param _tokenAddr the token the user wish to stake on
+    function stake(uint _amount, address _tokenAddr) isValidToken(_tokenAddr) external returns (bool){
+        require(_amount != 0);
+        require(ERC20(_tokenAddr).transferFrom(msg.sender,this,_amount));
+        
+        if (StakeMap[_tokenAddr][msg.sender].amount ==0){
+            StakeMap[_tokenAddr][msg.sender].amount = _amount;
+            userCummRewardPerStake[_tokenAddr][msg.sender] = tokenCummRewardPerStake[_tokenAddr];
+        }else{
+            claim(_tokenAddr, msg.sender);
+            StakeMap[_tokenAddr][msg.sender].amount = StakeMap[_tokenAddr][msg.sender].amount.add( _amount);
+        }
+        tokenTotalStaked[_tokenAddr] = tokenTotalStaked[_tokenAddr].add(_amount);
+        emit staked(_tokenAddr, msg.sender, _amount);
+        return true;
+    }
     
-    
-    event claimed(uint amount);
-    /**
-    * @dev claim dividends for a particular token that user has stake in
-    * @param _tokenAddr the token that the claim is made on
-    * @param _receiver the address which the claim is paid to
-    */
+    /// @notice This method to claim dividends for a particular token that user has stake in
+    /// @param  _tokenAddr the token that the claim is made on
+    /// @param _receiver the address which the claim is paid to
     function claim(address _tokenAddr, address _receiver) isValidToken(_tokenAddr)  public returns (uint) {
         uint stakedAmount = StakeMap[_tokenAddr][msg.sender].amount;
         //the amount per token for this user for this claim
@@ -135,20 +146,20 @@ contract Staking is Ownable {
         } else {
            require(ERC20(_tokenAddr).transfer(_receiver,claimableAmount));
         }
-        emit claimed(claimableAmount);
+        emit claimed(_tokenAddr, msg.sender, claimableAmount, _receiver);
         return claimableAmount;
 
     }
     
-    /**
-    * @dev Unstake
-    */
+    /// @notice This method to unstake a specific amount against LP Token
+    /// @param  _amount the amount to be unstake
+    /// @param _tokenAddr the token the user wish to unstake on
     function unstake(uint _amount, address _tokenAddr) isValidToken(_tokenAddr)  external returns(bool){
         require(StakeMap[_tokenAddr][msg.sender].amount >0 );
         claim(_tokenAddr, msg.sender);
         require(ERC20(_tokenAddr).transfer(msg.sender,_amount));
         tokenTotalStaked[_tokenAddr] = tokenTotalStaked[_tokenAddr].sub(_amount);
+        emit unstaked(_tokenAddr, msg.sender, _amount);
         return true;
     }
-    
 }
